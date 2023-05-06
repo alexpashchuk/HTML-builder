f3dd32dc519b34631691da4b17ce6de421e94ab5
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-05-06 16:49:41 +0300

    refactor:rename const name

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,12 +2,12 @@ const path = require('path');
 const fs = require('fs');
 const { stdout } = process;
 
-const ReadStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-ReadStream.on('data', (data) => {
+readableStream.on('data', (data) => {
   stdout.write(data);
 });
 
-ReadStream.on('error', (error) => {
+readableStream.on('error', (error) => {
   console.log(error.message);
 });
