a940e07f826dea09a9c0892539de58a55eb6bb7b
Aleksey Pashchuk <aleksey.pashchuk@gmail.com>
2023-05-08 16:59:38 +0300

    feat:implement task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
 
+const dirPathIn = path.join(__dirname, 'files');
+const dirPathOut = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+  fs.mkdir(dirPathOut, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  readdir(dirPathIn, { withFileTypes: true }).then((files) =>
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+          if (err) throw err;
+        });
+      }
+    })
+  );
+}
+copyDir();
